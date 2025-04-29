319e2b8bf2ca0f710cac29454c6eafe8747bd7fa
Keven Willians <149530293+kevenscharttz@users.noreply.github.com>
2025-04-29 16:13:49 -0300

    Add files via upload

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro programa PHP</title>
+</head>
+
+<body>
+    <h1>
+        <?php 
+            echo "Hello world \u{2728}";
+        ?>
+    </h1>
+    <p>Negando a maldição</p>
+</body>
+
+</html>
