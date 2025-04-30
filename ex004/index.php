1d3bd21c5f5910bb2f023b09f1e10924a96cdebd
Keven Willians <149530293+kevenscharttz@users.noreply.github.com>
2025-04-30 16:38:11 -0300

    Add files via upload

---
+++ ex004/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulação de Strings</title>
+</head>
+
+<body>
+    <?php 
+        //$nome = 'Keven';
+        //$emoji = '\u{1F418}';
+        //echo "Eu me chamo $nome, e adoro PHP $emoji";
+        //echo 'Eu me chamo $nome, e adoro PHP\u{1F418}';
+
+        //const ESTADO = "SC";
+
+        //echo "Eu moro no estado de ESTADO ";
+        //echo "EU moro no estado de ".ESTADO;
+
+        //$nome = "Rodrigo";
+        //$sobrenome = "Nogueira";
+        //echo "$nome \"Minotauro\" $sobrenome"
+        ?>
+    <pre>
+        <?php 
+        $nome = "Keven";
+        $sobrenome = "Willians";
+        $apelido = "Gafanhoto";
+
+        echo "$nome \n\t \"$apelido\" \n\t $sobrenome";
+        ?>
+    </pre>
+
+</body>
+
+</html>
